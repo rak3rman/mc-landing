634f3e9eccd39671396464168caf50454bf8b479
Radison Akerman <29184717+RAK3RMAN@users.noreply.github.com>
2020-02-11 17:43:20 -0600

    Initial commit

---
+++ routes/mainRoutes.js
@@ -0,0 +1,9 @@
+/*\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+App/Filename : BASE/routes/mainRoutes.js
+Author       : RAk3rman
+\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
+
+//Home Page Route - Main
+exports.homeRoute = function (req, res) {
+    res.render('pages/home.ejs', {title: 'Home', user: req.user})
+};
